1cd659434f376cffe7d8a16ddc55833ab3b0286c
MarioConejos <marioconejosb@gmail.com>
2017-06-13 20:12:31 +0200

    sadas

---
+++ Editory/src/main/java/com/editory/web/EditoryApplication.java
@@ -0,0 +1,12 @@
+package com.editory.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditoryApplication.class, args);
+	}
+}
